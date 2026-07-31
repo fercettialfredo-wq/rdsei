a3470e09e0b80eb6a4f59aafc1f567c9e706777f
fercettialfredo-wq <fercettialfredo@gmail.com>
2026-07-31 13:50:32 -0600

    Change app name in login box
    Updated app title and logo display.

--- app.js
+++ app.js
@@ -93,8 +93,8 @@ const SCREENS = {
                 <div style="text-align:center; margin-bottom:40px;">
                     <img src="icons/logo.png" alt="Logo" style="width: 120px; height: auto; margin-bottom: 15px;">
                     
-                    <h1 style="color:white; font-size:1.6rem; margin:0;">NUEVO NOMBRE APP</h1>
-                    <p style="color:#666; font-size:0.8rem;">Control de Rondines NFC</p>
+                    <h1 style="color:white; font-size:1.6rem; margin:0;">N</h1>
+                    <p style="color:#666; font-size:0.8rem;">Control de Rondines NFC</Ravens access
                 </div>
                 <div class="input-group">
                     <label>Usuario</label>
